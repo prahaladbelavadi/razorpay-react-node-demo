7963dd032117fea72781dec52d5fb91fa9889cf2
Prahalad Belavadi <github@belavadi.com>
2022-01-13 18:10:14 +0530

    initiall commit; Next time make incremental commits;

--- server/server.js
+++ server/server.js
@@ -1,1 +1,37 @@
-var instance = new Razorpay({  key_id: 'YOUR_KEY_ID',  key_secret: 'YOUR_KEY_SECRET',});
+require( 'dotenv' ).config();
+const express = require( 'express' );
+const Razorpay = require( 'razorpay' );
+
+const app = express();
+
+app.use( function ( req, res, next ) {
+    res.header( "Access-Control-Allow-Origin", "*" ); // update to match the domain you will make the request from
+    res.header( "Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept" );
+    next();
+} );
+
+// var instance = new Razorpay( { key_id: 'YOUR_KEY_ID', key_secret: 'YOUR_KEY_SECRET', } );
+// instance.payments.fetch( paymentId );
+
+var instance = new Razorpay( { key_id: `${process.env.key_id}`, key_secret: `${process.env.key_secret}` } );
+
+app.get( '/order', async ( req, res ) => {
+    // const { amount, currency } = req.body;
+
+    var options = {
+        amount: 20000000,
+        currency: "INR",
+        receipt: "receipt#1",
+        notes: {
+            key1: "value3",
+            key2: "value2"
+        }
+    };
+    instance.orders.create( options, function ( err, order ) {
+        res.json( order );
+    } );
+} );
+
+app.listen( 3500, () => {
+    console.log( 'Server up' );
+} );
--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
+import React, { useEffect, useState, useCallback } from 'react';
+
 import logo from './logo.svg';
 import './App.css';
-
+import useRazorpay from "react-razorpay";
 
 function App() {
+  const [order, setOrder] = useState();
+
+  const getOrderId = () => fetch( process.env.REACT_APP_BASEURL_TO_FETCH_RAZORPAY_ORDER_ID_FROM );
+
+  // const amount = Math.round(Math.random()*10000 *100);
+
+  useEffect( () => {
+    getOrderId().then( ( response ) => response.json() ).then( res => {
+      console.log( '====================================' );
+      console.log( res );
+      console.log( '====================================' );
+      setOrder( res.id );
+
+    } );
+
+  }, [] );
+
+  const Razorpay = useRazorpay();
+
+  const handlePayment = useCallback( () => {
+
+    const options = {
+      key: process.env.REACT_APP_RAZORPAY_PUBLIC_KEY_ID,
+      amount: 20000000,
+      currency: process.env.REACT_APP_CURRENCY_OF_PAYMENT_ACCEPTANCE,
+      name: process.env.REACT_APP_USER_DATA_NAME,
+      description: process.env.REACT_APP_RAZORPAY_DESCRIPTION,
+      image: process.env.REACT_APP_IMAGE_OF_BRAND,
+      order_id: order,
+      handler: ( res ) => {
+        alert( JSON.stringify( res ) );
+      },
+      prefill: {
+        name: process.env.REACT_APP_USER_DATA_NAME,
+        email: process.env.REACT_APP_USER_DATA_EMAIL,
+        contact: process.env.REACT_APP_USER_DATA_CONTACT,
+      },
+      // notes: JSON.parse( process.env.REACT_APP_USER_INFO_NOTES ),
+      theme: {
+        color: process.env.REACT_APP_BRAND_THEME_COLOR,
+      },
+    };
+
+    const rzpay = new Razorpay( options );
+    rzpay.open();
+  }, [Razorpay] );
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        {/* <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/prahaladbelavadi/razorpay-react-node-demo"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a> */}
-
+          <p>
+            Accept Payments via <code>Razorpay</code>
+          </p>
+          <p>
+            Built with <code> Node, Express and React</code>
+          </p>
+          github.com/prahaladbelavadi/razorpay-react-node-demo
+        </a>
+        <img src={logo} className="App-logo" alt="logo" />
+        <div className="App">
+          {order ? ( <div>Ready to Accept Payments
+            <br /> {"Order ID : " + order} <br />
+            <button onClick={handlePayment}>Pay</button>
+          </div>
+          ) : null}
+        </div>
       </header>
     </div>
   );
